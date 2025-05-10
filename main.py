f7c90d779671cdba046bc9a70bb3657fbd8ff51c
welin <119814308+WelingtonAguiar1987@users.noreply.github.com>
2025-05-10 08:17:13 -0400

    Inserindo Dataframe no Streamlit

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
-print('Hello Word')
-print('Welington')
+import yfinance as yf
+import pandas as pd
+import streamlit as st
+import numpy as np
+
+def dados_historicos(ticker, start_date, end_date, interval):
+    df = yf.download(tickers=ticker, start=start_date, end=end_date, interval=interval)
+    df.columns = df.columns.droplevel(1)
+    df['Return'] = df['Close'].pct_change()
+    df['Volatility'] = df['High'] - df['Low']
+    
+    return df
+
+
+ticker = "MNQ=F"
+start_date = "2024-03-15"
+end_date = "2025-06-20"
+interval = "1d"
+
+st.dataframe(dados_historicos(ticker, start_date, end_date, interval))
