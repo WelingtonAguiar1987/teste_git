a77f05260f82f18052b0e851177ebd63f874de17
welin <119814308+WelingtonAguiar1987@users.noreply.github.com>
2025-05-10 08:41:28 -0400

    Inserindo Desvio Padrão

--- main.py
+++ main.py
@@ -3,18 +3,21 @@ import pandas as pd
 import streamlit as st
 import numpy as np
 
-def dados_historicos(ticker, start_date, end_date, interval):
+def historical_data(ticker, start_date, end_date, interval):
     df = yf.download(tickers=ticker, start=start_date, end=end_date, interval=interval)
     df.columns = df.columns.droplevel(1)
     df['Return'] = df['Close'].pct_change()
     df['Volatility'] = df['High'] - df['Low']
-    
     return df
 
+def standard_deviation():
+    dp = np.std(historical_data(ticker, start_date, end_date, interval)['Return'] * 100)
+    return dp
 
-ticker = "MNQ=F"
+ticker = "MNQM25.CME"
 start_date = "2024-03-15"
 end_date = "2025-06-20"
 interval = "1d"
 
-st.dataframe(dados_historicos(ticker, start_date, end_date, interval))
+st.dataframe(historical_data(ticker, start_date, end_date, interval))
+st.write(f'Desvio Padrão: {standard_deviation():.2f}%')
